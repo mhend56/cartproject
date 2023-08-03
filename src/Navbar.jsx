46204c5d1b82c8befb68debd8e93b4788e615820
mhend56 <mhend56@gmail.com>
2023-08-03 15:39:10 +1200

    chg1

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-center">
-        <h4>useReducer</h4>
+        <h4>use Reducer</h4>
         <div className="nav-container">
           <FaCartPlus className="cart-icon" />
           <div className="amount-container">
